866b73cd857b1287ba56b746c0a51df5de080ea4
Octavian Taranu <taranu.toma.octavian@gmail.com>
2019-03-08 13:03:57 +0000

    ignore android double press

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -368,6 +368,7 @@ function drawPlayPause(boxSize) {
 
 // When the user clicks the mouse
 function mousePressed() {
+  if (event.type != 'touchstart') return true;
 
   var bound_top;
   var bound_bot;
