5dc82bd366765a3a8995811ea65065ba7fb52552
Octavian Taranu <taranu.toma.octavian@gmail.com>
2019-03-23 16:07:00 +0000

    sitemap (wip)

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var fs = require('fs');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var sitemapRouter = require('./routes/sitemap');
 
 console.log("Building app...");
 var app = express();
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/sitemap', sitemapRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/sitemap.js
+++ routes/sitemap.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.set('Content-Type', 'application/xml');
+  res.send('sitemap/sitemap.xml');
 });
 
 module.exports = router;
